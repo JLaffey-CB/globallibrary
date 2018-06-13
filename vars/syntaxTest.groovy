2460d9f4e84a4bd0fd3dba341ec8419a0e4f531d
John Laffey <jlaffey@cloudbees.com>
2018-06-13 09:05:31 -0700

    initial creation

---
+++ vars/syntaxTest.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+def call(syntaxTest){
+pipeline {
+    agent { node{label 'centos'}}
+    stages {
+    stage('Build') {
+            steps { container('centos')
+      {echo $APP}
+      {echo $IMAGE_TAG}
+      }
+    }
+  }
+  parameters {
+    string(name: 'APP', defaultValue: '', description: '')
+    string(name: 'REPOSITORY', defaultValue: '', description: '')
+    string(name: 'VERSION', defaultValue: "MyVersion", description: '')
+    string(name: 'IMAGE_TAG', defaultValue: "v${VERSION}/$REPOSITORY-${BUILD_NUMBER}", description: '')
+  }
+ }
+}
